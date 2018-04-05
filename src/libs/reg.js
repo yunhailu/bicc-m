ab1c9b0da40575f80d00001f908dab65e6e8f676
luyunhai <luyunhai@58ganji.com>
2018-04-05 21:01:27 +0800

    add 图片解析方法

--- src/libs/reg.js
+++ src/libs/reg.js
@@ -1,4 +1,16 @@
 /**
  * Created by yunhailu on 2018/4/5.
  */
+const REG = {
+    img: /<img[^>]+src=['"]([^'"]+)['"]+/g
+};
 
+export const getImageSrc = html => {
+    let result = [];
+    let temp;
+
+    while( (temp= REG.img.exec(html)) != null ) {
+        result.push(temp[1]);
+    }
+    return result;
+};
