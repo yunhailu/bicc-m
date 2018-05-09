034f3e0c944436417299d1b2c40cf18c1362b1b1
luyunhai <luyunhai@zhuanzhuan.com>
2018-05-09 16:39:04 +0800

    修改详情图片

--- src/components/Detail/Detail.vue
+++ src/components/Detail/Detail.vue
@@ -7,7 +7,11 @@
             <div class="detail-wrapper-container">
                 <div class="detail-wrapper-container-title">{{title}}</div>
                 <div class="detail-wrapper-container-cxt" v-html="html"></div>
+                <div class="detail-wrapper-container-image">
+                    <img :src="formatUrl(item.filename)" :key="index" v-for="(item, index) in images" />
+                </div>
             </div>
+            <copyright></copyright>
         </div>
     </div>
 </template>
@@ -18,6 +22,7 @@
 </style>
 
 <script>
+import _ from 'lodash';
 import DetailHeader from '../Common/Header/Header.vue';
 import Copyright from '../Common/Copyright/Copyright.vue';
 import { getDetail } from '../../libs/resourceApi';
@@ -28,10 +33,14 @@ export default {
     data () {
         return {
             title: '',
-            html: ''
+            html: '',
+            images: []
         };
     },
     methods: {
+        formatUrl (fileName) {
+            return `http://www.hibicc.com/sites/hibicc.com/files/${fileName}`;
+        },
         getDetail (id) {
             return getDetail({ id }).then(resp => {
                 console.log(resp);
@@ -40,6 +49,10 @@ export default {
                 // this.html = setAVoid(this.html);
                 // this.html = setTable(this.html);
                 console.log(this.html);
+                const field_image = resp.field_image && resp.field_image.und && resp.field_image.und.length ? resp.field_image.und : [];
+                const field_testimonial = resp.field_testimonial && resp.field_testimonial.und && resp.field_testimonial.und.length ? resp.field_testimonial.und : [];
+                this.images = _.concat(this.images, field_image, field_testimonial);
+                console.log('this.image=======', field_image, field_testimonial, this.images);
             });
         },
         init () {
